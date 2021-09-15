5adf8567c23cfc8e7d94660e58580d7c6c3048cb
Loic Diridollou <loic.diridollou@gmail.com>
2021-09-14 20:38:01 -0700

    Add material for the flask app

---
+++ app/__init__.py
@@ -0,0 +1,30 @@
+# application/__init__.py
+from flask import Flask, abort, jsonify, request
+from experiments.cron_jobs_test import main_fn
+from threading import Thread
+
+
+def create_app(test_config=None):
+    """Creating the app and db
+    """
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(SECRET_KEY='dev')
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+
+    @app.route('/')
+    def home():
+        return jsonify({'message': 'healthy'}), 200
+
+    thread = Thread(target=main_fn)
+    thread.daemon = True
+    thread.start()
+
+    return app
+
+
+app = create_app()
