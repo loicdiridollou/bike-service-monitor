d2d2244bd6b6390b7d941a86a7de77419dac73bb
Loic Diridollou <loic.diridollou@gmail.com>
2021-09-14 20:39:15 -0700

    BSM-3 Add config file for email tests

--- experiments/email_test.py
+++ experiments/email_test.py
@@ -1,16 +1,22 @@
 import smtplib, ssl
+import yaml
+
+
+with open('config.yaml') as config_file:
+        config = yaml.full_load(config_file)['data']
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
 sender_email = "ld.app.testing@gmail.com"  # Enter your address
 receiver_email = "loic.diridollou@gmail.com"  # Enter receiver address
-password = ""
+password = config["password"]
 message = """\
-Subject: Hi there
+Subject: Bike Service Notification
 
-This message is sent from Python."""
+This is the state of the near-by station of 199 New Montgomery St."""
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+def email_sender():
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
