ded9094ebc3ca9583ad6c6f1d86aa3689bdfb86e
Loic Diridollou <loic.diridollou@gmail.com>
2021-09-14 21:20:43 -0700

    Improve message display

--- experiments/email_test.py
+++ experiments/email_test.py
@@ -1,21 +1,32 @@
 import smtplib, ssl
 import yaml
+import os
+from experiments.data_acq_test import get_results
 
-
-with open('config.yaml') as config_file:
+if not os.environ.get("EMAIL_PASSWORD"):
+    with open('config.yaml') as config_file:
         config = yaml.full_load(config_file)['data']
+        os.environ.update(config)
 
-port = 465  # For SSL
-smtp_server = "smtp.gmail.com"
-sender_email = "ld.app.testing@gmail.com"  # Enter your address
-receiver_email = "loic.diridollou@gmail.com"  # Enter receiver address
-password = config["password"]
-message = """\
-Subject: Bike Service Notification
 
-This is the state of the near-by station of 199 New Montgomery St."""
 
 def email_sender():
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "ld.app.testing@gmail.com"  # Enter your address
+    receiver_email = "loic.diridollou@gmail.com"  # Enter receiver address
+    password = os.environ.get("EMAIL_PASSWORD")
+    message = """\
+    Bike Service Notification
+
+This is the state of the near-by station of 199 New Montgomery St.\n"""
+    
+    values = get_results()
+    for elem in values:
+        llist = [field + ': ' + str(elem[field]) for field in elem]
+        message += " | ".join(llist)
+        message += "\n"
+
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
