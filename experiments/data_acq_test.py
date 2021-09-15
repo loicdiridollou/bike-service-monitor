0a7db43fc5c5290038ab373266349eecf12f1d0e
Loic Diridollou <loic.diridollou@gmail.com>
2021-09-14 21:20:34 -0700

    Improve message display

--- experiments/data_acq_test.py
+++ experiments/data_acq_test.py
@@ -1,11 +1,17 @@
 import requests
 
-url = 'https://gbfs.baywheels.com/gbfs/en/station_status.json'
+FIELDS = ['station_id', 'is_renting', 'num_bikes_available', 'num_ebikes_available']
 
-source = requests.get(url).json()
+def get_results():
+    url = 'https://gbfs.baywheels.com/gbfs/en/station_status.json'
+    source = requests.get(url).json()
 
-llist = source['data']['stations']
+    llist = source['data']['stations']
 
-for i in range(len(llist)):
-    if llist[i]['station_id'] == '445': 
-        print(llist[i])
+    values = []
+    for i in range(len(llist)):
+        if (llist[i]['station_id'] == '445' 
+            or llist[i]['station_id'] == "25"
+            or llist[i]['station_id'] == "363"): 
+            values.append({field: llist[i][field] for field in FIELDS})
+    return values
