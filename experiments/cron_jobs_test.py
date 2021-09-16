fddf25bae3f21414c7c410fb0596791d84c733df
Loic Diridollou <loic.diridollou@gmail.com>
2021-09-16 08:13:49 -0700

    gcer

--- experiments/cron_jobs_test.py
+++ experiments/cron_jobs_test.py
@@ -16,7 +16,7 @@ def main_fn():
         hour="6", minute="22", second="0", timezone="US/Pacific"
     )
     trigger3 = CronTrigger(
-        hour="8", minute="13", second="0", timezone="US/Pacific"
+        hour="8", minute="15", second="0", timezone="US/Pacific"
     )
     scheduler.add_job(
         email_sender,
