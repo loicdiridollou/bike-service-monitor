a0e87edb7b884cd44385f3a8af0a3c92a675aac8
Loic Diridollou <loic.diridollou@gmail.com>
2021-09-18 15:12:53 -0700

    Change version number

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages, setup
 
 setup(name='src',
-      version='0.1',
+      version='0.0.1',
       description='Package source of the different modules',
       author='Loic Diridollou',
       author_email='loic.diridollou@gmail.com',
