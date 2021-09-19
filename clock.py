2cb11433a03a0cef3ba1d929cbae09623849a52a
Loic Diridollou <loic.diridollou@gmail.com>
2021-09-19 12:54:02 -0700

    hjmvtjrio

--- clock.py
+++ clock.py
@@ -1,4 +1,7 @@
-from experiments.email_test import email_sender
+#from experiments.email_test import email_sender
+import os
+
 
 if __name__ == "__main__":
-    email_sender()
+    #email_sender()
+    print(os.environ.get('EMAIL_PASSWORD'))
