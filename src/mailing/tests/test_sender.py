41ed2c9dc16976ad3cd0faa930b7d3949bfcf57b
Loic Diridollou <loic.diridollou@gmail.com>
2021-09-21 18:25:31 -0700

    Try workflow with failing test

--- src/mailing/tests/test_sender.py
+++ src/mailing/tests/test_sender.py
@@ -5,3 +5,7 @@ import mailing.sender as ms
 def test_email_sender():
     """Test function for the email_sender"""
     ms.email_sender(message="Random message", test_mode=True)
+
+
+def test_error():
+    assert False
