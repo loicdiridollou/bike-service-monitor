88ca09e586229fcd50095f5e3cecd77fb620e4a5
Loic Diridollou <loic.diridollou@gmail.com>
2021-09-26 18:57:08 -0700

    Fix pylint

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,12 @@
-# application/__init__.py
-from re import L
-from apscheduler.triggers import cron
-from mailing.sender import email_sender
-from flask import Flask, jsonify, request, abort
-from scheduling.cron_jobs import main_fn
+"""application/__init__.py"""
 from threading import Thread
 from datetime import datetime
 import os
 import yaml
+from flask import Flask, jsonify, request, abort
+from scheduling.cron_jobs import main_fn
+from mailing.sender import email_sender
+
 
 DEFAULT_CONFIG = "config/config.yaml"
 
@@ -15,7 +14,7 @@ DEFAULT_CONFIG = "config/config.yaml"
 thread = Thread(target=main_fn)
 thread.daemon = True
 thread.start()
-    
+
 
 def create_app(test_config=None):
     """Creating the app and db
@@ -40,17 +39,17 @@ def create_app(test_config=None):
     def print_var():
         return jsonify({"debug": app.debug,
                         'os_env': os.environ.get('WERKZEUG_RUN_MAIN')})
-    
+
     @app.route('/email')
     def send_email():
         email_sender()
         return jsonify({'message': 'success'}), 200
-    
+
     @app.route('/times')
     def list_times():
         with open("config/config.yaml", "rb") as config_file:
             cron_times = yaml.full_load(config_file)["cron_times"]
-        return jsonify({"message": "success", 
+        return jsonify({"message": "success",
                         "times": cron_times}), 200
 
     @app.route('/times', methods=['POST'])
@@ -58,29 +57,29 @@ def create_app(test_config=None):
         body = request.get_json()
         if body is None:
             abort(400)
-        with open(config_fn, "rb") as config_file:
+        with open(config_fn, "rb", encoding="utf-8") as config_file:
             config_data = yaml.full_load(config_file)
         names = sorted(config_data["cron_times"].keys())
         new_num = int(names[-1][4:])+1
         config_data["cron_times"]["time" + str(new_num)] = body
 
-        with open(config_fn, "w") as config_file:
+        with open(config_fn, "w", encoding="utf-8") as config_file:
             yaml.dump(config_data, config_file)
-        
+
         return jsonify({"message": "succes"}), 200
 
     @app.route("/times/<time_name>", methods=["DELETE"])
     def delete_times(time_name, config_fn=DEFAULT_CONFIG):
-        with open(config_fn, "rb") as config_file:
+        with open(config_fn, "rb", encoding="utf-8") as config_file:
             config_data = yaml.full_load(config_file)
         names = config_data["cron_times"]
         if time_name not in names:
             abort(400)
         del config_data["cron_times"][time_name]
 
-        with open(config_fn, "w") as config_file:
+        with open(config_fn, "w", encoding="utf-8") as config_file:
             yaml.dump(config_data, config_file)
-        
+
         return jsonify({"message": "succes", "deleted_time": time_name}), 200
 
     @app.errorhandler(400)
@@ -92,4 +91,4 @@ def create_app(test_config=None):
 
     return app
 
-app = create_app()
+application = create_app()
