19b4eb826b2697d46313bfeca8e7ad68dc2dea7a
Loic Diridollou <loic.diridollou@gmail.com>
2021-10-17 08:20:48 -0700

    Fixing lint

--- application/tests/test_routes.py
+++ application/tests/test_routes.py
@@ -1,38 +1,44 @@
 """Testing the different routes of the application"""
 import pytest
-import datetime
 from application import create_app
+# pylint: disable=W0621
 
 
 @pytest.fixture
 def client():
+    """Defining client fixture"""
     app = create_app({'TESTING': True})
 
-    with app.test_client() as client:
-        yield client
+    with app.test_client() as client_fixt:
+        yield client_fixt
 
 
 def test_landing(client):
-    rv = client.get('/')
-    assert {'message': 'healthy'} == rv.get_json()
+    """Testing landing page route"""
+    request = client.get('/')
+    assert {'message': 'healthy'} == request.get_json()
 
 
 def test_time(client):
-    rv = client.get('/time')
-    assert rv.status_code == 200
-    assert isinstance(rv.get_json()['time'], str)
+    """Testing time route"""
+    request = client.get('/time')
+    assert request.status_code == 200
+    assert isinstance(request.get_json()['time'], str)
 
 
 def test_times(client):
-    rv = client.get('/times')
-    assert rv.status_code == 200
-    
+    """Testing times route"""
+    request = client.get('/times')
+    assert request.status_code == 200
+
 
 def test_error404(client):
-    rv = client.get('/timegcbrrs')
-    assert rv.status_code == 404
+    """Testing error handler 404"""
+    request = client.get('/timegcbrrs')
+    assert request.status_code == 404
 
 
 def test_error400(client):
-    rv = client.delete('/times/gcbrrs')
-    assert rv.status_code == 400
+    """Testing error handler 400"""
+    request = client.delete('/times/gcbrrs')
+    assert request.status_code == 400
