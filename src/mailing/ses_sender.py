5a19f528e768badd4a9a78b879e407f020a5e163
Loic Diridollou <loic.diridollou@gmail.com>
2022-05-18 20:20:01 -0700

    Fixing lint

--- src/mailing/ses_sender.py
+++ src/mailing/ses_sender.py
@@ -1,21 +1,23 @@
+"""Encapsulation of SES email notification."""
 import boto3
 from botocore.exceptions import ClientError
 
 def send_email(recipients=[]):
+    """Main encapsulation."""
     AWS_REGION = "us-west-1"
     client = boto3.client('ses', region_name=AWS_REGION)
     CHARSET = "UTF-8"
     SENDER = "ld-app-testing (AWS) <ld.app.testing@gmail.com>"
-    
+
     message = "Hello my name is SES"
-    
+
     SUBJECT = "Bike Service Notification"
     BODY_TEXT = (message)
-                
+
     try:
         response = client.send_email(
             Destination={
-                'ToAddresses': 
+                'ToAddresses':
                     recipients,
             },
             Message={
@@ -31,9 +33,8 @@ def send_email(recipients=[]):
                 },
             },
             Source=SENDER,
-        
         )
-    # Display an error if something goes wrong.	
+    # Display an error if something goes wrong.
     except ClientError as e:
         print(e.response['Error']['Message'])
     else:
