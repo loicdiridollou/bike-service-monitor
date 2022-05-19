a275d62fca5e30fe014a3ad1fb06ff618c3540cd
Loic Diridollou <loic.diridollou@gmail.com>
2022-05-18 20:13:25 -0700

    Update test

--- src/mailing/tests/test_sender.py
+++ src/mailing/tests/test_sender.py
@@ -1,9 +1,14 @@
 """Test functions for the mailing.sender module"""
 import pytest
 import mailing.sender as ms
-
+import mailing.ses_sender as mses
 
 @pytest.mark.skip(reason="Password issues")
 def test_email_sender():
     """Test function for the email_sender"""
     ms.email_sender(message="Random message", test_mode=True)
+
+
+def test_ses_sender():
+    """Test function for the SES email sender."""
+    mses.send_email(recipients=["ld.app.testing@gmail.com"])
